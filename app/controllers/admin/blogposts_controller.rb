ebdc8ce5e63786f7f37b33a44f2a27248f978b9c
Cody Ma <cody_ma@brown.edu>
2014-05-30 23:24:04 -0400

    admin blogging off to a good start

--- app/controllers/admin/blogposts_controller.rb
+++ app/controllers/admin/blogposts_controller.rb
@@ -3,12 +3,32 @@ class Admin::BlogpostsController < ApplicationController
   layout "admin"
 
   def index
+    @blogpost = current_admin.blogposts.build if signed_in?
     @blogposts = current_admin.blogposts.where(public: 'true')
+  end
+
+  def create
+    @blogpost = current_admin.blogposts.build(blogpost_params)
+    if @blogpost.save
+      redirect_to admin_path
+    end
+  end
+
+  def destroy
+    @blogpost = Blogpost.find((params[:id]))
+    if @blogpost.present?
+      @blogpost.destroy
+    end
+    redirect_to admin_path
   end
 
   private
 
     def signed_in
       redirect_to signin_path unless signed_in?
+    end
+
+    def blogpost_params
+      params.require(:blogpost).permit(:title, :content, :public)
     end
 end
