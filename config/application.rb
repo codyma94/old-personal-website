2b5ff684f02ba09828cebdf50898cbec7b567a34
Cody Ma <cody_ma@brown.edu>
2014-06-02 23:42:36 -0400

    added fonts locallygit add .!

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module PersonalWebsite
   class Application < Rails::Application
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,3 +27,4 @@ module PersonalWebsite
     # config.i18n.default_locale = :de
   end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,17 @@ Blogpost.delete_all
 
 cody = Admin.create( {id: 1, name: "Cody Ma", email: "cody_ma@brown.edu", password: "foobar12", password_confirmation: "foobar12"} )
 
-cody.blogposts.create(title: "Hello World!",
+cody.blogposts.create(title: "## Hello World!",
                       content:
-                      "= Markdown Test
-                      This is my very first post using Markdown
+                      "# Markdown Test
+                      This is my very *first* post using Markdown
                       I learned it from [Google](http:/www.google.com)",
                      public: 'true',
                      created_at: Time.now - 1.day)
 
-cody.blogposts.create(title: "Second post",
+cody.blogposts.create(title: "## Second post",
                       content:
-                     "=== List testing
+                     "### List testing
                      * First item
                      * second item
                      * third item",
