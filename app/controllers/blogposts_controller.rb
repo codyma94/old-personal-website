74a16b04f23d1c097f56d75d24e5a59329594901
Cody Ma <cody_ma@brown.edu>
2014-06-18 23:32:01 -0400

    add blog controller

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -1,6 +1,6 @@
-class StaticPagesController < ApplicationController
+class BlogpostsController < ApplicationController
 
-  def home
+  def index
     @blogposts = Blogpost.all.where(:public => true)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   resources :sessions, only: [:new, :create, :destroy]
+  resources :blog, :controller => "blogposts"
 
   namespace :admin do
     match '', to: 'blogposts#index', via: 'get', as: '/'
