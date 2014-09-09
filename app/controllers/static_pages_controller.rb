bf60e77b407d5dec5175217c03a88b302e50f729
Cody Ma <cody_ma@brown.edu>
2014-09-09 11:27:54 -0400

    fix broken dl resume link

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,7 @@ class StaticPagesController < ApplicationController
 
   def download_resume
     send_file(
-      "#{Rails.root}/public/Resume.pdf",
+      "#{Rails.root}/public/cody_ma_resume.pdf",
       filename: "cody_ma_resume.pdf",
       type: "application/pdf"
     )
