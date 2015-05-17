9eadafa057bfd44d1529799eecc91079efb7ff50
Cody Ma <cody_ma@brown.edu>
2015-05-17 18:10:17 -0500

    bug fixes, refactor section expanding js

--- app/assets/javascripts/page_expand.js
+++ app/assets/javascripts/page_expand.js
@@ -1,5 +1,5 @@
 // expands section based on hash of url
-window.onload = function(){
+$(window).on('load hashchange', function(){
   var hash = (window.location.hash).replace('#', '');
   if (hash.length != 0) {
     var $main = $("#bl-main");
@@ -8,42 +8,23 @@ window.onload = function(){
 
     switch (hash) {
       case "about":
-        console.log("about");
         $child = $("#bl-main section:nth-child(1)")
         break;
       case "resume":
-        console.log("resume");
         $child = $("#bl-main section:nth-child(2)")
         break;
       case "blog":
-        console.log("blog");
         $child = $("#bl-main section:nth-child(3)")
         break;
       case "contact":
-        console.log("contact");
         $child = $("#bl-main section:nth-child(4)")
         break;
     }
 
-    var transEndEventNames = {
-       'WebkitTransition' : 'webkitTransitionEnd',
-       'MozTransition' : 'transitionend',
-       'OTransition' : 'oTransitionEnd',
-       'msTransition' : 'MSTransitionEnd',
-       'transition' : 'transitionend'
-    };
-    var transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ];
-
-    $child.data( 'open', false ).removeClass( 'bl-expand' ).on( transEndEventName, function( event ) {
-      if( !$( event.target ).is( 'section' ) ) return false;
-      $( this ).off( transEndEventName ).removeClass( 'bl-expand-top' );
-    });
-
-    $main.removeClass( 'bl-expand-item' );
-    if( !$child.data( 'open' ) ) {
-      $child.data( 'open', true ).addClass( 'bl-expand bl-expand-top' );
-      $main.addClass( 'bl-expand-item' );
+    $("#bl-main>section.bl-expand").removeClass();
+    $child.data('open', false).removeClass('bl-expand');
+    if(!$child.data('open')) {
+      $child.data('open', true ).addClass('bl-expand bl-expand-top');
     }
   }
-
-}
+});
--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,78 +1,37 @@
 var Boxlayout = (function() {
 
   function init() {
-    var $el = $( "#bl-main" );
+    var $el = $("#bl-main");
     var $sections = $el.children("section");
-    var transEndEventNames = {
-       'WebkitTransition' : 'webkitTransitionEnd',
-       'MozTransition' : 'transitionend',
-       'OTransition' : 'oTransitionEnd',
-       'msTransition' : 'MSTransitionEnd',
-       'transition' : 'transitionend'
-    };
 
-    // transition end event name
-    var transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ];
-
-    // support css transitions
-    var supportTransitions = Modernizr.csstransitions;
-
-    initEvents($el, $sections, transEndEventName, transEndEventNames, supportTransitions);
+    initEvents($el, $sections);
   }
 
-  function initEvents($el, $sections, transEndEventName, transEndEventNames, supportTransitions) {
-    $sections.each( function() {
-
+  function initEvents($el, $sections) {
+    $sections.each(function() {
       var $section = $(this);
-
       // expand the clicked section and scale down the others
-      $section.on( 'click', function() {
-        var newHash = $section.attr('id');
-
-        if( !$section.data( 'open' ) ) {
-          $section.data( 'open', true ).addClass( 'bl-expand bl-expand-top' );
-          $el.addClass( 'bl-expand-item' );
+      $section.on('click', function() {
+        if(!$section.data('open') ) {
+          $section.data('open', true).addClass('bl-expand bl-expand-top');
+          $el.addClass('bl-expand-item');
         }
-        window.location.hash = newHash;
-
-      }).find( 'i.bl-icon-close' ).on( 'click', function(e) {
-
-        // close the expanded section and scale up the others
-        $section.data( 'open', false ).removeClass( 'bl-expand' ).on( transEndEventName, function( event ) {
-          if( !$( event.target ).is( 'section' ) ) return false;
-          $( this ).off( transEndEventName ).removeClass( 'bl-expand-top' );
-        } );
-
-        if( !supportTransitions ) {
-          $section.removeClass( 'bl-expand-top' );
-        }
-
-        $el.removeClass( 'bl-expand-item' );
-        window.location.hash = "";
-
-        return false;
-
+        window.location.hash = $section.attr('id');
+      }).find('i.bl-icon-close').on('click', function(e) {
+        closeSection($el, $section);
       });
 
-      //refactor at some point
+      // close section on 'esc'
       $(document).on('keydown', function(e){
-        if (e.keyCode == '27') {
-          $section.data( 'open', false ).removeClass( 'bl-expand' ).on( transEndEventName, function( event ) {
-            if( !$( event.target ).is( 'section' ) ) return false;
-            $( this ).off( transEndEventName ).removeClass( 'bl-expand-top' );
-          });
-
-          if( !supportTransitions ) {
-            $section.removeClass( 'bl-expand-top' );
-          }
-
-          $el.removeClass( 'bl-expand-item' );
-          window.location.hash = "";
-        }
+        if (e.keyCode == '27') closeSection($el, $section);
       });
     });
   }
-
-  return { init : init };
-
+  return {init : init};
 })();
+
+var closeSection = (function($el, $section) {
+  $section.data('open', false).removeClass();
+  $el.removeClass('bl-expand-item');
+  window.location.hash = "";
+});
