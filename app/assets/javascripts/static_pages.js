f5955c7cd1c033a1352ce00a2225f917e10eb457
Cody Ma <cody_ma@brown.edu>
2015-05-18 00:28:06 -0500

    fix section js bugs and refactor

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,15 +1,12 @@
 var Boxlayout = (function() {
 
   function init() {
-    var $el = $("#bl-main");
+    var $el = $( "#bl-main" );
     var $sections = $el.children("section");
 
-    initEvents($el, $sections);
-  }
-
-  function initEvents($el, $sections) {
     $sections.each(function() {
       var $section = $(this);
+
       // expand the clicked section and scale down the others
       $section.on('click', function() {
         if(!$section.data('open') ) {
@@ -18,19 +15,22 @@ var Boxlayout = (function() {
         }
         window.location.hash = $section.attr('id');
       }).find('i.bl-icon-close').on('click', function(e) {
-        closeSection($el, $section);
+        sectionClose($el, $section);
+        return false;
       });
 
       // close section on 'esc'
       $(document).on('keydown', function(e){
-        if (e.keyCode == '27') closeSection($el, $section);
+        if (e.keyCode == '27') sectionClose($el, $section);
       });
     });
   }
+
   return {init : init};
+
 })();
 
-var closeSection = (function($el, $section) {
+var sectionClose = (function($el, $section) {
   $section.data('open', false).removeClass();
   $el.removeClass('bl-expand-item');
   window.location.hash = "";
